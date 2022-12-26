6916e027b12d9d321bba68afc86a90e093d272ea
tchamalsha <tharu.chamalsha@gmail.com>
2022-12-26 13:50:55 +0530

    edit recently visited fine

--- src/views/RecentlyVisited.vue
+++ src/views/RecentlyVisited.vue
@@ -2,6 +2,13 @@
     <div class="conatiner p-3">
       <h1>Recently Visited News</h1>
       <hr>
+      <div v-if="!list.length">
+            <!-- Element to be rendered if data is null -->
+            <div class="image justify-content-center d-flex">
+                <img src="../assets/img/empty.svg" alt="Paris" class="center" >
+            </div>
+            <p>No Data Available</p>
+      </div>
       <div class="row mt-3">
           <ListCard 
             v-for="item,index in list" 
@@ -41,4 +48,13 @@ export default {
 
 <style>
 @import '../assets/main.css';
+
+
+img{
+  width: 40%;
+}
+p{
+  text-align: center;
+  font-size: 2rem;
+}
 </style>
